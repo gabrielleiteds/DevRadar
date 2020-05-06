e0b79876f952fc7f6d0ff0acaf709ee756beee7e
leitegs <leitedasilvag@gmail.com>
2020-05-05 21:19:14 -0300

    using cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,10 +2,11 @@
 const express = require('express');
 const mongoose = require('mongoose')
 const routes = require('./routes');
-
+const cors = require('cors'); 
 const app = express();
 
 //config routes
+app.use(cors());
 app.use(express.json());
 app.use(routes); 
 
