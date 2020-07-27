2a6563a2365f15415beb1c8640349b7b1f1bb0f5
leitegs <leitedasilvag@gmail.com>
2020-07-27 16:05:48 -0300

    organization the files

--- web/src/App.js
+++ web/src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 
-import api from './services/api'; 
+import api from './services/api';
 
 import './global.css';
 import './App.css';
@@ -8,44 +8,44 @@ import './Main.css';
 import './Sidebar.css';
 
 //componentes
-import DevForm from './components/DevForm'; 
-import DevItem from './components/DevItem'; 
+import DevForm from './components/DevForm';
+import DevItem from './components/DevItem';
 
 //componente App
 function App() {
 
-  const [devs, setDevs] = useState([]); 
+  const [devs, setDevs] = useState([]);
 
   async function handleAddDev(data) {
 
-    const response = await api.post('/devs', data); 
+    const response = await api.post('/devs', data);
 
-    alert('usuario cadastrado');  
+    alert('usuario cadastrado');
 
     setDevs([...devs, response.data])
   }
 
   useEffect(() => {
     async function loadDevs() {
-      const response = await api.get('/devs'); 
+      const response = await api.get('/devs');
 
-      setDevs(response.data); 
+      setDevs(response.data);
     }
 
-    loadDevs(); 
+    loadDevs();
   }, [])
 
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        < DevForm onSubmit={handleAddDev}/>
+        < DevForm onSubmit={handleAddDev} />
       </aside>
 
       <main>
         <ul>
-          {devs.map(dev => ( <DevItem key={dev._id} dev={dev} /> ))}
-          
+          {devs.map(dev => (<DevItem key={dev._id} dev={dev} />))}
+
 
         </ul>
       </main>
